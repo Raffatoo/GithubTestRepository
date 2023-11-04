1151ec37fc40d50207de5c0d07de238cf4585d8e
Raffatoo <rtorres_33@hotmail.com>
2023-11-04 03:58:28 -0600

    Initial Commit

---
+++ GithubTestRepository/GithubTestRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestRepositoryApp.swift
+//  GithubTestRepository
+//
+//  Created by Rafael Torres on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
